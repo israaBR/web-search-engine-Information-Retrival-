6a838f0ad25e621174b5fa60b7f2bb34371986b0
hoso99 <hosoabdelnaby@gmail.com>
2022-03-14 23:47:36 -0800

    Html Parser is done set it as a fun

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using mshtml;
 
 namespace WindowsFormsApp1
 {
@@ -12,11 +14,49 @@ namespace WindowsFormsApp1
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
+            string link;
+            string Paragraph;
+            string title;
+            string head;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+             List<string> HtmlTask(string htmllink)
+            {
+                HTMLDocument hh = new HTMLDocument();
+                hh.write(htmllink);
+                IHTMLElementCollection ele = hh.links;
+                
+                foreach (IHTMLElement e in ele)
+                {
+                    link = (string)e.getAttribute("href", 0);
+                    Paragraph = (string)e.getAttribute("p", 0);
+                    title = (string)e.getAttribute("title", 0);
+                    head = (string)e.getAttribute("head", 0);
+
+                }
+                List<string> arr = new List<string>();
+                arr.Add(head);
+                arr.Add(title);
+                arr.Add(Paragraph);
+                arr.Add(link);
+
+                return arr;
+            }
+
+
         }
+
+
+
     }
+
+
 }
+
