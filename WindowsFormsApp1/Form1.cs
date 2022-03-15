d4e98cba452a27396fb3ff6960acdd2023e31bae
Israa <israa.abdelaal.2000@gmail.com>
2022-03-16 00:55:38 +0200

    URL filters and normalization functions added

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,92 +7,210 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using System.Data;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Net;
+using mshtml;
+
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Esraa\source\repos\WindowsFormsApp1\WindowsFormsApp1\database.mdf;Integrated Security=True");
+        private static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\faculty\web-search-engine-Information-Retrival-\WindowsFormsApp1\database.mdf;Integrated Security=True");
+        private List<String> visitedURLs, toBeVisitedURLs, currentlyVisitingURLs;
+        private static List<String> _BlockedUrls = new List<String>();
+
         public Form1()
         {
+            //initialize URL lists
+            visitedURLs = new List<String>();
+            toBeVisitedURLs = new List<String>();
+            currentlyVisitingURLs = new List<String>();
+
             InitializeComponent();
-           
         }
+        private void textBox1_TextChanged(object sender, EventArgs e){ }
+        private void label1_Click(object sender, EventArgs e){ }
+        private void label3_Click(object sender, EventArgs e){ }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-   
-        private void label3_Click(object sender, EventArgs e)
-        {
-          
-
-        }
 
         private void crawel_button_Click(object sender, EventArgs e)
         {
             con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "insert into url_data values ('" + url_text + "','" + url2_text + "')";
 
+            // crawling processing steps
+            // 1.get seed URL(when crawling starts first time)
+            String seedURL = url_text.Text;
+
+            
+            // 2.fetch the document at the URL
+            String Rstring = get_URL_content(url_text.Text);
+            ISet<string> Links = GetContent(Rstring);//gets the links only
+
+            // 3.Parse the URL – HTML parser
+
+            // 4. Extract links from it to other docs (URLs)
+
+
+
+
+
+            con.Close();
+        }
+        private void pause_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private String get_URL_content(String URL)
+        {
             String Rstring;
-
             WebRequest myWebRequest;
             WebResponse myWebResponse;
-            String URL = url_text.Text;
 
             myWebRequest = WebRequest.Create(URL);
             myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
 
-            Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet
-                                                                      //and save it in the stream
+            Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet and save it in the stream
 
             StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
             Rstring = sreader.ReadToEnd();//reads it to the end
-            ISet<string> Links = GetContent(Rstring);//gets the links only
-
 
             streamResponse.Close();
             sreader.Close();
             myWebResponse.Close();
-
-
-
-
-
-
-
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("added successfully");
+            return Rstring;
         }
-
-
         private ISet<string> GetContent(String Rstring)
         {
             Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
-
             ISet<string> newLinks = new HashSet<string>();
             foreach (var match in regexLink.Matches(Rstring))
             {
                 if (!newLinks.Contains(match.ToString()))
                     newLinks.Add(match.ToString());
             }
-
             return newLinks;
+        }
+        
+        private void parse_robots_file(String URL)
+        {
+            Uri CurrentURL = new Uri(URL);
+            string RobotsTxtFile = "http://" + CurrentURL.Authority + "/robots.txt";
+            string FileContents = get_URL_content(RobotsTxtFile);
+            if (!String.IsNullOrEmpty(FileContents))
+            {
+                string[] fileLines = FileContents.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                bool Applied = false;
+                foreach (string line in fileLines)
+                {
+                    RobotCommand CommandLine = new RobotCommand(line);
+                    switch (CommandLine.Command)
+                    {
+                        case "user-agent":   // User-Agent
+                            if ((CommandLine.UserAgent.IndexOf("*") >= 0))
+                            {
+                                // these rules are applied to our useragent so on next line if its a DISALLOW we will add the URL
+                                // to our array of URls we cannot access
+                                Applied = true;
+                            }
+                            else
+                            {
+                                Applied = false;
+                            }
+                            break;
+                        case "disallow":   // Disallow
+                            if (Applied)
+                            {
+                                // Only add to blocked if the URL is not blank
+                                if (CommandLine.Url.Length > 0)
+                                {
+                                    _BlockedUrls.Add(CommandLine.Url.ToLower());
+                                }
+                            }
+                            break;
+                        default:
+                            // comment/allow/sitemap/empty/unknown/error
+                            break;
+                    }
+                }
+            }
+        }
+        private bool URL_is_allowed(String URL)
+        {
+            // If no URLS stored in blocked URLs list then all URLs are allowed
+            if (_BlockedUrls.Count == 0) return true;
 
-            
+            // Convert string into an Uri object to easily access the relative path excluding the host and domain etc.
+            Uri checkURL = new Uri(URL);
+            URL = checkURL.AbsolutePath.ToLower();
+
+            if (URL == "/robots.txt")
+            {
+                return false;
+            }
+            else
+            {
+                // loop our array checking for substring matches
+                foreach (string blockedURL in _BlockedUrls)
+                {
+                    if (URL.Length >= blockedURL.Length)
+                    {
+                        if (URL.Substring(0, blockedURL.Length) == blockedURL)
+                        {
+                            // found a DISALLOW command
+                            return false;
+                        }
+                    }
+                }
+            }
+            //the URL is not disallowed
+            return true;
+        }
+        private String URL_normalization(String URL)
+        {
+            // 1.put URL in lower case
+            String finalURL = URL.ToLower();
+
+            // 2.remove '/' from the end of the URL
+            if (finalURL[finalURL.Length - 1].Equals('/'))
+                finalURL.Remove(finalURL.Length - 1);
+
+            // 3.removing 'www.' part if exists
+            if (finalURL.Contains("www."))
+                finalURL.Replace("www.", "");
+
+            // 4.remove fragment part'#' if exists
+            int fragmentPosition = finalURL.IndexOf('#');
+            if(fragmentPosition > 0) //fragment exists
+                finalURL = finalURL.Substring(0, fragmentPosition);
+
+            // 5.remove default port(80 for http/ 443 for https)
+            if (finalURL.Contains(":80"))
+                finalURL.Replace(":80", "");
+            if (finalURL.Contains(":443"))
+                finalURL.Replace(":443", "");
+
+            // 6.remove 's' from 'https'
+            if (finalURL.Contains("https"))
+                finalURL.Replace("https", "http");
+
+            return finalURL;
+        }
+        private bool URL_is_exist(String URL)
+        {
+            //check if URL has been crawled already and stored in database
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "select * from url_data where url = '" + URL + "'";
+            Int32 count = (Int32)cmd.ExecuteScalar();
+            con.Close(); 
+
+            if (count > 0)
+                return true;
+            return false;
         }
     }
 }
---
+++ WindowsFormsApp1/RobotCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class RobotCommand
+    {
+        private string _Command;
+        private string _Url = string.Empty;
+        private string _Useragent = string.Empty;
+
+        public RobotCommand(string commandline)
+        {
+            int commentPosition = commandline.IndexOf('#');
+            if (commentPosition == 0)
+            {
+                // whole line is a comment
+                _Command = "COMMENT";
+            }
+            else
+            {
+                // there is a comment on the line so remove it
+                if (commentPosition >= 0)
+                {
+                    commandline = commandline.Substring(0, commentPosition);
+                }
+                // now if we have an instruction
+                if (commandline.Length > 0)
+                {
+                    string[] lineArray = commandline.Split(':');
+                    _Command = lineArray[0].Trim().ToLower();
+                    if (lineArray.Length > 1)
+                    {
+                        // set appropriate property depending on command type
+                        if (_Command == "user-agent")
+                        {
+                            _Useragent = lineArray[1].Trim();
+                        }
+                        else
+                        {
+                            _Url = lineArray[1].Trim();
+                            // if the URL is a full URL e.g sitemaps then it will contain
+                            // a : so add to URL
+                            if (lineArray.Length > 2)
+                            {
+                                _Url += ":" + lineArray[2].Trim();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Command
+        {
+            get { return _Command; }
+        }
+        public string Url
+        {
+            get { return _Url; }
+        }
+        public string UserAgent
+        {
+            get { return _Useragent; }
+        }
+
+    }
+}
